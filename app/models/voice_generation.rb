cd9a7560474e3acbd1cf19f326b0e968f8ba1e05
Priyadharshini .R <58815997+Priya-dharshini-r@users.noreply.github.com>
2025-12-25 17:22:35 +0530

    map user to voice generation

--- app/models/voice_generation.rb
+++ app/models/voice_generation.rb
@@ -1,16 +1,15 @@
 class VoiceGeneration < ApplicationRecord
-    enum status: {
+  belongs_to :user
+
+  enum :status, {
     pending: 0,
     processing: 1,
     completed: 2,
     failed: 3
   }
-  
-  belongs_to :user
 
-  validates :text, presence: true, length: { maximum: 5_000 }
+  validates :text, presence: true
   validates :voice, presence: true
   validates :language, presence: true
   validates :provider, presence: true
-  validates :status, presence: true
 end
